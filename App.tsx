511420683b696cf1a054c61fec7ea30a86c49f04
Michael White <michaelwhite404@gmail.com>
2022-04-15 15:36:09 -0400

    Home page empty state

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { AccountScreen, HomeScreen, PastScreen } from "./src/screens";
-import { AntDesign, FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import "react-native-reanimated";
 
 interface RootStackParams {
   Home: undefined;
@@ -29,6 +31,11 @@ export default function App() {
           name="Home"
           component={HomeScreen}
           options={{
+            headerRight: () => (
+              <TouchableOpacity onPress={() => {}}>
+                <Ionicons name="ios-add" size={30} color={"#0a84ff"} style={{ marginRight: 10 }} />
+              </TouchableOpacity>
+            ),
             tabBarIcon: ({ color, size }) => <AntDesign name="home" size={size} color={color} />,
           }}
         />
@@ -39,7 +46,7 @@ export default function App() {
             headerTitle: "Past Sessions",
             tabBarLabel: "Past Sessions",
             tabBarIcon: ({ color, size }) => (
-              <FontAwesome5 name="backward" size={size} color={color} />
+              <Ionicons name="ios-play-skip-back-outline" size={size} color={color} />
             ),
           }}
         />
@@ -49,16 +56,12 @@ export default function App() {
           options={{
             headerTitle: "Account",
             tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="account-circle" size={size} color={color} />
+              <Ionicons name="ios-person-outline" size={size} color={color} />
             ),
           }}
         />
       </RootStack.Navigator>
     </NavigationContainer>
-    // <View style={styles.container}>
-    //   <Text>Lions Den App</Text>
-
-    // </View>
   );
 }
 
