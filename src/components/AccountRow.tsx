d28660d7b9d56e908d485244e5e947a37f85e441
Michael White <michaelwhite404@gmail.com>
2022-04-23 15:08:28 -0400

    Added account info and account row

---
+++ src/components/AccountRow.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+export default function AccountRow({
+  icon,
+  text,
+  arrow,
+  color,
+}: {
+  text?: string;
+  arrow?: boolean;
+  icon?: ReactNode;
+  color?: string;
+}) {
+  return (
+    <TouchableOpacity>
+      <View style={styles.row}>
+        <View style={styles.rowLeft}>
+          {icon}
+          <Text style={[styles.text, { color }]}>{text}</Text>
+        </View>
+        {arrow && <Feather name="chevron-right" size={24} color="gray" />}
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20,
+    borderBottomColor: "#e5e7eb",
+    borderBottomWidth: 1,
+  },
+  rowLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  text: {
+    fontSize: 17,
+    marginLeft: 20,
+    fontWeight: "500",
+  },
+});
--- src/screens/AccountScreen.tsx
+++ src/screens/AccountScreen.tsx
@@ -1,12 +1,55 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import useAuth from "../hooks/useAuth";
+import AccountRow from "../components/AccountRow";
 
 export default function AccountScreen() {
+  const { user } = useAuth();
   return (
-    <View>
-      <Text>Account Screen</Text>
+    <View style={styles.container}>
+      {/* Profile */}
+      <View style={styles.profile}>
+        {/* Image */}
+        <Image style={styles.image} source={{ uri: user?.image }} />
+        {/* Text */}
+        <View>
+          <Text style={styles.name}>{user?.fullName}</Text>
+          <Text style={styles.title}>{user?.title}</Text>
+        </View>
+      </View>
+      <View style={{ borderTopColor: "#e5e7eb", borderTopWidth: 1 }}>
+        <AccountRow
+          text="Log out"
+          icon={<Ionicons name="log-out-outline" size={24} color="red" />}
+          color="red"
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+  },
+  profile: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 20,
+    marginVertical: 15,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 500,
+    marginRight: 15,
+  },
+  name: {
+    fontWeight: "600",
+  },
+  title: {
+    color: "#757575",
+  },
+});
